c20980ff3100df5c1c9c6c54c26c18f5b26e15bd
Luan-73 <luannguyennhat7@gmail.com>
2026-03-08 08:57:59 +0700

    Update 007.py

--- 007.py
+++ 007.py
@@ -1077,11 +1077,9 @@ elif menu == "Game":
                     spin_placeholder.markdown(
                         f"## Đang quay ... {random.choice(st.session_state.new_prizes)}"
                     )
-               result = random.choices(
-    st.session_state.new_prizes,
-    weights=st.session_state.weights,
-    k=1
-)[0]
+                result = random.choice(st.session_state.new_prizes,
+                                       weight = st.session_state.weights,
+                                       k = 1)[0]
                 spin_placeholder.empty()
                 st.balloons()
                 st.success(f"Chúc mừng bạn đã trúng: **{result}**")
@@ -1091,7 +1089,6 @@ elif menu == "Game":
             st.session_state.new_prizes = []
             st.session_state.weights = []
             st.success("Đã reset")
-
 
 
 
